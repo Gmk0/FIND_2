2c9afebf9172d34e3972cc511f5919f4db4d5b51
gmk0 <mkgeorges63@gmail.com>
2023-04-26 00:12:11 +0100

    save files

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -4,14 +4,18 @@
 @include('include.head')
 
 <body class="flex flex-col justify-between overflow-x-hidden bg-gray-50 custom-scrollbar dark:bg-gray-900"
-    x-data="{navOpen: false,notificationActive: false, isLoading:true, scrolledFromTop: false}" :class="{
+    x-data="setup()" :class="{
             'overflow-hidden': navOpen,
             'overflow-scroll': !navOpen
 
-        }">
+        }" x-init="$refs.loading.classList.add('hidden')">
 
 
-
+    <div x-ref="loading"
+        class="fixed inset-0 z-[200] flex items-center justify-center text-white bg-black bg-opacity-50"
+        style="backdrop-filter: blur(14px); -webkit-backdrop-filter: blur(14px)">
+        Loading.....
+    </div>
 
 
     @livewire('user.navigation.navigation')
@@ -30,6 +34,23 @@
     @livewire('user.navigation.footer')
 
     @include('include.script')
+
+    <script>
+        const setup = () => {
+            return {
+            loading: true,
+            navOpen: false,
+            i
+            notificationActive:false,
+            scrolledFromTop:false,
+            toggleSidbarMenu() {
+            this.isSidebarOpen = !this.isSidebarOpen
+            },
+            isSettingsPanelOpen: false,
+            isSearchBoxOpen: false,
+            }
+}
+    </script>
 
 </body>
 
