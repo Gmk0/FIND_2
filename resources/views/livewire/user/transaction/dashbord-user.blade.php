85537befdb66ae79517d003d911bc9dc1c27d10d
gmk0 <mkgeorges63@gmail.com>
2023-04-30 00:40:29 +0100

    change dash

--- resources/views/livewire/user/transaction/dashbord-user.blade.php
+++ resources/views/livewire/user/transaction/dashbord-user.blade.php
@@ -1,6 +1,6 @@
 <div class="p-2">
     <div
-        class="flex flex-col min-h-screen gap-6 p-6 mx-auto bg-gray-100 md:max-w-7xl md:container px-auto dark:bg-gray-900">
+        class="flex flex-col min-h-screen gap-6 md:p-6 mx-auto bg-gray-100 md:max-w-7xl md:container px-auto dark:bg-gray-900">
         <!-- start::Stats -->
         <div class="grid grid-cols-1 gap-4 px-auto md:grid-cols-2 lg:grid-cols-4">
             <a href="{{route('commandeUser')}}"
@@ -246,6 +246,74 @@
 
                 </tbody>
             </table>
+        </div>
+
+
+        <div class="flex flex-col mt-6">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px- lg:px-8">
+                    <div class="overflow-hidden border-b border-gray-200 rounded-md shadow-md">
+                        <table class="min-w-full overflow-x-scroll divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                        Name
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                        Title
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                        Status
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                        Role
+                                    </th>
+                                    <th scope="col" class="relative px-6 py-3">
+                                        <span class="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <template x-for="i in 10" :key="i">
+                                    <tr class="transition-all hover:bg-gray-100 hover:shadow-lg">
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="flex-shrink-0 w-10 h-10">
+                                                    <img class="w-10 h-10 rounded-full"
+                                                        src="https://avatars0.githubusercontent.com/u/57622665?s=460&u=8f581f4c4acd4c18c33a87b3e6476112325e8b38&v=4"
+                                                        alt="" />
+                                                </div>
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium text-gray-900">Ahmed Kamel</div>
+                                                    <div class="text-sm text-gray-500">ahmed.kamel@example.com</div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
+                                            <div class="text-sm text-gray-500">Optimization</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <span
+                                                class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
+                                                Active
+                                            </span>
+                                        </td>
+                                        <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">Admin</td>
+                                        <td class="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                        </td>
+                                    </tr>
+                                </template>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
 
 
