7c1cf926c1df9d9d5963de565744bf78c44d2e4a
georges mk <98753718+Gmk0@users.noreply.github.com>
2023-05-29 09:48:38 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
 
     <div x-ref="loading"
-        class="fixed inset-0 z-[200] flex items-center justify-center text-white bg-black bg-opacity-50">
+        class="fixed inset-0 z-[200] flex items-center justify-center text-white bg-black bg-opacity-50"
         style="backdrop-filter: blur(14px); -webkit-backdrop-filter: blur(14px)">
         Chargement.....
     </div>
