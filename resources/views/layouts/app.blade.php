eb3f6b09be9cc48db4b7c31b1919aa2e231b8d60
georges mk <98753718+Gmk0@users.noreply.github.com>
2023-05-29 09:48:07 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,11 @@
             'overflow-hidden': navOpen,
             'overflow-scroll': !navOpen
 
-        }" x-init="$refs.loading.classList.add('hidden')">>
+        }" x-init="$refs.loading.classList.add('hidden')">
 
 
     <div x-ref="loading"
-        class="fixed inset-0 z-[200] flex items-center justify-center text-white bg-black bg-opacity-50"
+        class="fixed inset-0 z-[200] flex items-center justify-center text-white bg-black bg-opacity-50">
         style="backdrop-filter: blur(14px); -webkit-backdrop-filter: blur(14px)">
         Chargement.....
     </div>
