174b626158b519a90bb7d34827eff8ad2a47b1ec
gmk0 <mkgeorges63@gmail.com>
2023-06-04 18:36:09 +0100

    send

--- resources/views/livewire/user/navigation/navigation.blade.php
+++ resources/views/livewire/user/navigation/navigation.blade.php
@@ -206,7 +206,7 @@ $fixed="";
                     <button
                         class="text-gray-800 rounded-md dark:text-white focus:outline-none focus:shadow-outline-purple"
                         @click="toggleSearch" aria-label="Toggle color mode">
-                        <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -241,12 +241,19 @@ $fixed="";
 
             </div>
         </nav>
-        <nav class="hidden lg:flex">
+        <nav class="hidden lg:flex ">
 
-            <ul>
+            <ul class="flex gap-2">
 
                 @auth
 
+
+                <li class="">
+
+                    @livewire('user.navigation.notifications-component')
+
+
+                </li>
 
                 <li class="relative ml-3">
 
@@ -300,8 +307,8 @@ $fixed="";
                 <button class="text-gray-800 rounded-md dark:text-white focus:outline-none focus:shadow-outline-purple"
                     @click="isSearchBoxOpen=!isSearchBoxOpen" aria-label="Toggle color mode">
 
-                    <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
+                    <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
